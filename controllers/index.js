d99101be9b24857a344152fbeb2156b6014bfbd2
Johnny <jaani.matharu@gmail.com>
2021-06-02 10:00:08 -0400

    add comment few error fixing

--- controllers/index.js
+++ controllers/index.js
@@ -3,10 +3,12 @@ const router = require('express').Router();
 const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
+const singlepostRoutes = require('./single-post.js');
 
 //add 
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
+router.use('/single-post', singlepostRoutes);
 
 module.exports = router;
